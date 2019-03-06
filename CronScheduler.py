9a26cad9d08dfe9a7e3c7a3715ffa0d12ac45baf
Lucas Thomas <robertlucasthomas@gmail.com>
2019-03-06 10:46:31 -0600

    Update cronscheduler
    Write cronjob to cron

--- CronScheduler.py
+++ CronScheduler.py
@@ -6,8 +6,8 @@ Created on Wed Mar 06 10:00:44 2019
 """
 
 import os
-import datetime
 from crontab import CronTab
 
 my_cron = CronTab(user='robertlucasthomas')
-job = my_cron.new(command = 'python ' + os.path.join(os.getcwd(), 'PullCurrentData' + '.' + 'py'))
+pulldatajob = my_cron.new(command = 'python ' + os.path.join(os.getcwd(), 'PullCurrentData' + '.' + 'py'))
+pulldatajob.minute.every(1)
